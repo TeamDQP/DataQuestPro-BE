dac18f2d469002be863439d1172e057d985b70c7
Water-Stone <wngjsthd8@naver.com>
2023-08-22 18:03:42 +0900

    email template 하드코딩 테스트

---
+++ notification/email_templates.py
@@ -0,0 +1,18 @@
+# purposes = {'purpose':'...'}
+# purpose = {'title':'...', 'body':'...'}
+
+purposes = {'register':{'title':'Welcome to DataQuestPro!',
+            'body':
+                '''<!DOCTYPE html>
+                    <html>
+                    <head>
+                        <title>Welcome to DataQuestPro!</title>
+                    </head>
+                    <body>
+                        <h1>Hi, {{ username }}!</h1>
+                        <h2>This is TeamDQP.</h2>
+                        <p>Thank you for your registeration. 환영합니다!</p>
+                    </body>
+                    </html>
+                '''}
+            }
