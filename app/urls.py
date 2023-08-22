e1aea65ed9939bc057f92f95e58685d042128551
Water-Stone <wngjsthd8@naver.com>
2023-08-22 10:42:31 +0900

    including notification urls

--- app/urls.py
+++ app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('surveys/', include('surveys.urls')),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('notification/', include('notification.urls')),
 ]
