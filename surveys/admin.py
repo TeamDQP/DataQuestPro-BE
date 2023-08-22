5614f635ec5c8b64433b192389a10432296f3e12
beerbank3 <yg050140528@gmail.com>
2023-08-22 16:32:13 +0900

    admim 페이지 등록

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Survey, Category, Tag, Question, Answer, UserAnswer
 
 # Register your models here.
+admin.site.register(Survey) # admin 페이지에 등록
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(UserAnswer)
