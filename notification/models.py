60b47cdb519775853afc68934086fd895d2c873f
Water-Stone <wngjsthd8@naver.com>
2023-08-23 17:13:37 +0900

    modify EmailRecord model

--- notification/models.py
+++ notification/models.py
@@ -13,7 +13,7 @@ class EmailRecord(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)    # 요청보내는 유저
     title = models.CharField(max_length=255)    # 이메일 제목
     body = models.TextField()   # 본문 내용
-    targets = models.TextField()    # 수신인 목록을 공백으로 구분하여 문자열로 저장(View에서 split()으로 사용)
+    targets = models.TextField()    # 수신인 목록을 공백으로 구분하여 문자열로 저장(리스트에서 .join하여 저장)
     # 이메일 송신 목적
     PURPOSE_TYPES = [
         ('register', 'Register'),   # 회원가입
