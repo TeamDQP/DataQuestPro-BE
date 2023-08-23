ec7377bfefac609477d6ca70c1e880e302916fda
Water-Stone <wngjsthd8@naver.com>
2023-08-23 13:50:06 +0900

    add email templates

--- notification/email_templates.py
+++ notification/email_templates.py
@@ -2,14 +2,17 @@
 # purpose = {'title':'...', 'body':'...'}
 
 purposes = {'register':{'title':'Welcome to DataQuestPro!',
-            'body':
+            'body1':
                 '''<!DOCTYPE html>
                     <html>
                     <head>
                         <title>Welcome to DataQuestPro!</title>
                     </head>
                     <body>
-                        <h1>Hi, {{ username }}!</h1>
+                        <h1>Hi, 
+                ''',
+            'body2':
+                '''!</h1>
                         <h2>This is TeamDQP.</h2>
                         <p>Thank you for your registeration. 환영합니다!</p>
                     </body>
