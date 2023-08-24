23230a4b07bad0b698c94058092bb65163aca15f
Water-Stone <wngjsthd8@naver.com>
2023-08-24 14:20:46 +0900

    delete EmailRecord user

--- notification/models.py
+++ notification/models.py
@@ -10,7 +10,6 @@ class EmailManager(models.Manager):
 
 # https://docs.djangoproject.com/en/4.2/topics/email/
 class EmailRecord(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)    # 요청보내는 유저
     title = models.CharField(max_length=255)    # 이메일 제목
     body = models.TextField()   # 본문 내용
     targets = models.TextField()    # 수신인 목록을 공백으로 구분하여 문자열로 저장(리스트에서 .join하여 저장)
