34ba34bc7f87fe530bd0fea003458135abe407fe
Sunse Kwon <sunse523@gmail.com>
2023-08-24 13:09:53 +0900

    email_opt_in field added to user and survey_close_email in signals updated

---
+++ notification/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from .views import SendEmailView
+
+urlpatterns = [
+    # path('send_email/', SendEmailView.as_view(), name='send_email'),
+]
--- user/models.py
+++ user/models.py
@@ -35,6 +35,7 @@ class User(AbstractUser):
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    email_opt_in = models.BooleanField(default=True)
     last_login = models.DateTimeField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
