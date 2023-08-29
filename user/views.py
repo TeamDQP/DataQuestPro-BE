a1c1ecff4af81ee3beda03fba2baf5d332a51041
Sunse Kwon <sunse523@gmail.com>
2023-08-29 17:43:01 +0900

    profile, user delete, login modified

--- user/views.py
+++ user/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer, ProfileSerializer
+from .serializers import UserSerializer, ProfileSerializer, CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from .models import Profile
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 
 class RegisterView(APIView):
     def post(self, request):
@@ -15,6 +17,10 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 class LogoutView(APIView):
@@ -44,7 +50,6 @@ class JWTValidationView(APIView):
             return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-
 class EmailVerification(APIView):
     def get(self, request, pk):
         try:
@@ -55,7 +60,7 @@ class EmailVerification(APIView):
                 return Response('wake', status=status.HTTP_200_OK)
             return Response('Email has already been verified.', status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            return Response({'error':e}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': e}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserUpdate(APIView):
@@ -73,6 +78,16 @@ class UserUpdate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDelete(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        user = User.objects.get(email=request.user)
+        user.is_active = False
+        user.save()
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class ProfileWrite(APIView):
@@ -110,7 +125,14 @@ class ProfileUpdate(APIView):
         profile = Profile.objects.get(user=request.user)
         serializer = ProfileSerializer(profile, data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            # print(request.data)
+            # print(serializer.validated_data['profileimage'])
+            # print(type(serializer.validated_data['profileimage']))
+            if serializer.validated_data['profileimage']:
+                serializer.save()
+            else:
+                # print('null')
+                serializer.save(profileimage=profile.profileimage)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
